975d1a8850c55d080dfb0bdc01b101dcbbae3f90
Taurit <git@taurit.pl>
2017-10-30 18:50:13 +0100

    Proof of concept

---
+++ Taurit.TotalCommander.F4Editor/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Taurit.TotalCommander.F4Editor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(args.Length);
+            foreach (var arg in args)
+            {
+                Console.WriteLine(arg);
+            }
+            Console.ReadKey();
+        }
+    }
+}
