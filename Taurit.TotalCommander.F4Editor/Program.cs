81c78014fb8c98299080849c12c1f5c36908b33d
Taurit <git@taurit.pl>
2017-10-30 21:18:42 +0100

    Add feature: open file in system's default editor for filetype

--- Taurit.TotalCommander.F4Editor/Program.cs
+++ Taurit.TotalCommander.F4Editor/Program.cs
@@ -1,18 +1,27 @@
-﻿using System;
+﻿using System.Diagnostics;
+using System.IO;
 
 namespace Taurit.TotalCommander.F4Editor
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine(args.Length);
-            foreach (var arg in args)
-            {
-                Console.WriteLine(arg);
-            }
-            Console.ReadKey();
+            Debug.Assert(args.Length == 1, "Expecting 1 argument which is a full path to the file we want to edit.");
+            var fileToEditPath = args[0];
+            var program = new Program();
+            program.OpenFileInDefaultEditor(fileToEditPath);
+        }
+
+        private void OpenFileInDefaultEditor(string fileToEditPath)
+        {
+            var pi = new ProcessStartInfo(fileToEditPath);
+            pi.Arguments = Path.GetFileName(fileToEditPath);
+            pi.UseShellExecute = true;
+            pi.WorkingDirectory = Path.GetDirectoryName(fileToEditPath);
+            pi.FileName = fileToEditPath;
+            pi.Verb = "EDIT";
+            Process.Start(pi);
         }
     }
 }
